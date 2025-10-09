f916d72e5293dbb8052a93366b1409440b6d20d1
unknown <goodness.kolawole@pau.edu.ng>
2025-10-10 00:47:27 +0100

    (week 2 project)

---
+++ week-2/practice_1.rs
@@ -0,0 +1,15 @@
+fn main() {
+	println!("Welcome to CSC 101!");
+
+    println!("The course learning outcomes is as follows:");
+
+    println!("1.  Distinguish between computer and computer programming,/n
+              2.  Develop some techniques in computer science,/n
+              3.  Understand the different areas of studies in computer science,/n 
+              4.  Conversant with application of computer science,/n 
+              5.  Navigate the career prospectrs in computer science./n 
+              6.  Conversant with computer programming concepts ");
+  }
+
+
+    
