fc1784ee1180ca69392dd69dbe02617f0f71824c
unknown <goodness.kolawole@pau.edu.ng>
2025-10-23 13:35:06 +0100

    (week 4 practices)

---
+++ week-4/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\HomePC\Documents\g.kolawoleCOS101\week-4\practice_6\target\debug\practice_6.exe: C:\Users\HomePC\Documents\g.kolawoleCOS101\week-4\practice_6\src\main.rs
---
+++ week-4/practice_8/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // while true 
+    let mut x = 0;
+    loop{
+        x+=1;
+        print!("x={}",x );
+
+        if x==15{
+            break;
+        }
+    }
+}
